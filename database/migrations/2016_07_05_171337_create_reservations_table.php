465170811f611eb630f2594a28eab9d94f7c0021
Felipe Medeiros <felipenovaesmedeiros@gmail.com>
2016-07-05 14:31:50 -0300

    Updt migration create Reservation

--- database/migrations/2016_07_05_171337_create_reservations_table.php
+++ database/migrations/2016_07_05_171337_create_reservations_table.php
@@ -13,25 +13,25 @@ class CreateReservationsTable extends Migration
     public function up()
     {
         Schema::create('reservations', function (Blueprint $table) {
-            $table->increments('id')->unsigned();
-            $table->integer('teacher_id')->unsigned();//id do professor
-            $table->integer('class_id');//id da turma
-            $table->integer('classroom_id');//id da sala de aula
-            $table->integer('projector_id')->nullable();//id do projetor
-            $table->integer('notebook_id')->nullable();//id do notebook
-            $table->integer('speaker_id')->nullable();//id da caixa de som
-            $table->date('date')->unsigned();
-            $table->string('period', 20)->unsigned();
-            $table->string('block', 20)->unsigned();
-            $table->timestamps();
+            $table->increments  ('id')              ->unsigned();
+            $table->integer     ('teacher_id')      ->unsigned();   //id do professor
+            $table->integer     ('class_id');                       //id da turma
+            $table->integer     ('classroom_id');                   //id da sala de aula
+            $table->integer     ('projector_id')    ->nullable();   //id do projetor
+            $table->integer     ('notebook_id')     ->nullable();   //id do notebook
+            $table->integer     ('speaker_id')      ->nullable();   //id da caixa de som
+            $table->date        ('date')            ->unsigned();   //data da reserva
+            $table->string      ('period', 20)      ->unsigned();   //periodo da reserva
+            $table->string      ('block', 20)       ->unsigned();   //bloco de aula da reserva
+            $table->timestamps  ();
 
             //fk
-            $table->foreign('teacher_id')->references('id')->on('teachers');
-            $table->foreign('class_id')->references('id')->on('classes');
-            $table->foreign('classroom_id')->references('id')->on('classroms');
-            $table->foreign('projector_id')->references('id')->on('projectors');
-            $table->foreign('notebook_id')->references('id')->on('notebooks');
-            $table->foreign('speaker_id')->references('id')->on('speakers');
+            $table->foreign('teacher_id')   ->references('id')->on('teachers');
+            $table->foreign('class_id')     ->references('id')->on('classes');
+            $table->foreign('classroom_id') ->references('id')->on('classroms');
+            $table->foreign('projector_id') ->references('id')->on('projectors');
+            $table->foreign('notebook_id')  ->references('id')->on('notebooks');
+            $table->foreign('speaker_id')   ->references('id')->on('speakers');
         });
     }
 
@@ -42,6 +42,15 @@ class CreateReservationsTable extends Migration
      */
     public function down()
     {
+        Schema::table('reservations', function ($table) {
+            $table->dropForeign('reservations_teacher_id_foreign');
+            $table->dropForeign('reservations_class_id_foreign');
+            $table->dropForeign('reservations_classroom_id_foreign');
+            $table->dropForeign('reservations_projector_id_foreign');
+            $table->dropForeign('reservations_notebook_id_foreign');
+            $table->dropForeign('reservations_speaker_id_foreign');
+        });
+
         Schema::drop('reservations');
     }
 }
