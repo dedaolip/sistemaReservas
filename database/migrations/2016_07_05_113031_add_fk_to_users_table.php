14ca25a5db74061043f913433708295709d5db65
Felipe Medeiros <felipenovaesmedeiros@gmail.com>
2016-07-05 08:44:07 -0300

    migration add_fk_to_users criado

---
+++ database/migrations/2016_07_05_113031_add_fk_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFkToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->integer('people_id')->unsigned();
+
+            $table->foreign('people_id')->references('id')->on('people');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function ($table) {
+            $table->dropForeign('users_people_id_foreign');
+        });
+    }
+}
