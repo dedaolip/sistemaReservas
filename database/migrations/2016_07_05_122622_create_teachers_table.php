0588c058bc6d069198865aa1e880533ab48845ca
Felipe Medeiros <felipenovaesmedeiros@gmail.com>
2016-07-05 12:31:52 -0300

    model e migration Teacher criados

---
+++ database/migrations/2016_07_05_122622_create_teachers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('teachers', function ($table) {
+            $table->dropForeign('teachers_user_id_foreign');
+        });
+
+        Schema::drop('teachers');
+    }
+}
