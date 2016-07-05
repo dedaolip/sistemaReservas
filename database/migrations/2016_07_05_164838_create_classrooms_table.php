729df05d9f84c93553284a8c7eb465513d986ebd
Felipe Medeiros <felipenovaesmedeiros@gmail.com>
2016-07-05 13:49:50 -0300

    model e migration Classroom criados

---
+++ database/migrations/2016_07_05_164838_create_classrooms_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassroomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classrooms', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('name', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('classrooms');
+    }
+}
