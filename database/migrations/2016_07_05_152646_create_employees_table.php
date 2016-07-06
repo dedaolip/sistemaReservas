0dffa8e20eec44bc24a328a1ae12cb52e0497a91
Felipe Novaes Medeiros <felipenovaesmedeiros@gmail.com>
2016-07-05 21:29:54 -0300

    Updt migration create_employees

--- database/migrations/2016_07_05_152646_create_employees_table.php
+++ database/migrations/2016_07_05_152646_create_employees_table.php
@@ -16,6 +16,7 @@ class CreateEmployeesTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
+            $table->string('office', 50);
             $table->timestamps();
         });
     }
